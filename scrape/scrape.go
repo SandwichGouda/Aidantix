8398f66e6de08e575802f1e776c018b1398ae2e2
SandwichGouda <william.driot@gmail.com>
2025-02-23 14:40:08 +0100

    Code refacored scrape.go

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -11,14 +11,20 @@ import (
 func ScrapeDay() int {
 	resp, err := http.Get("https://cemantix.certitudes.org/")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("scrape.go : ", err)
 		return 0
 	}
+
+	var (
+		method1_criticalfail bool
+		method2_criticalfail bool
+	)
+
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("scrape.go : ", err)
 		return 0
 	}
 	page := string(body)
@@ -26,32 +32,44 @@ func ScrapeDay() int {
 	i_1 := strings.Index(page, "data-puzzle-number=")
 	if i_1 == -1 {
 		fmt.Println(`"scrape.go : data-puzzle-number=" not found on page"`)
-		return 0
+	} else {
+		i_1 += 20
 	}
-	i_1 += 20
 
 	day_1, err := strconv.Atoi(page[i_1 : i_1+4])
 	if err != nil {
 		fmt.Println("scrape.go : Failed to convert string page[i_1 : i_1+4] = ", page[i_1:i_1+4], "to int.")
-		return 0
+		method1_criticalfail = true
 	}
 
 	i_2 := strings.Index(page, `id="puzzle-num">`)
 	if i_2 == -1 {
 		fmt.Println(`scrape.go : id="puzzle-num"> not found on page"`)
-		return 0
+		method2_criticalfail = true
+	} else {
+		i_2 += 16
 	}
-	i_2 += 16
 
 	day_2, err := strconv.Atoi(page[i_2 : i_2+4])
 	if err != nil {
 		fmt.Println("scrape.go : Failed to convert string page[i_2 : i_2+4] = ", page[i_2:i_2+4], "to int.")
-		return 0
+		method2_criticalfail = true
 	}
 
-	if day_1 != day_2 {
-		fmt.Println("scrape.go : day_1 =", day_1, " != ", day_2, " = day_2")
+	switch {
+	case method1_criticalfail && method2_criticalfail:
+		fmt.Println("scrape.go : both methods failed")
 		return 0
+	case method1_criticalfail:
+		fmt.Println("scrape.go : method 1 failed, returning day_2")
+		return day_2
+	case method2_criticalfail:
+		fmt.Println("scrape.go : method 2 failed, returning day_1")
+		return day_1
+	default:
+		if day_1 != day_2 {
+			fmt.Println("scrape.go : day_1 =", day_1, " != ", day_2, " = day_2 ; returning day_1")
+		}
+		return day_1
 	}
-	return day_1
 }
