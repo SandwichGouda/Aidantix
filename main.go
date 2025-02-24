bcc5a0b57f12c3a3103d5771765cc856a8f1a06c
SandwichGouda <william.driot@gmail.com>
2025-02-24 14:49:41 +0100

    Add the two-methods-plus-hardcode feature

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"aidantix/req"
 	"aidantix/scrape"
 	"aidantix/scrape_goquery"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 type Response struct {
@@ -26,8 +29,20 @@ func real_main() {
 
 	day = scrape_goquery.ScrapeDay()
 	if day == 0 {
-		fmt.Println("scrape_goquery.ScrapeDay() returned 0. Please type in the puzzle day number :")
+		fmt.Print("scrape_goquery.ScrapeDay() returned 0. Please type in the puzzle day number : ")
+		reader := bufio.NewReader(os.Stdin)
+		nb, _ := reader.ReadString('\n')
+		nb = nb[:len(nb)-1]
 
+		var err error
+		day, err = strconv.Atoi(nb)
+
+		for err != nil {
+			fmt.Print("Input convertion to int type failed. Please type in the puzzle day number : ")
+			nb, _ = reader.ReadString('\n')
+			nb = nb[:len(nb)-1]
+			day, err = strconv.Atoi(nb)
+		}
 	}
 
 	fmt.Println("main : day", day)
