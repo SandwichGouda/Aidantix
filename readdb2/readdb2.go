7cf47fcde4a86c32642b06e0e0ad97660f24c2cb
SandwichGouda <william.driot@gmail.com>
2025-04-21 15:58:13 +0200

    Small fix regarding readdb1 (comma separation not handled)

--- readdb2/readdb2.go
+++ readdb2/readdb2.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	// "fmt"
 )
 
 func ImportDatabase() []string {
@@ -14,14 +13,6 @@ func ImportDatabase() []string {
 	if err != nil {
 		log.Fatal("readdb2.go : Failed to read database \n", err)
 	}
- 
-	str_db_with_freq := strings.Split(string(db),"\n")
-	
-	str_db := make([]string,10000)
 
-	for i,wd := range str_db_with_freq {
-		str_db[i] = strings.Split(wd,",")[1]
-	}
-
-	return str_db
+	return strings.Split(string(db),"\n")
 }
